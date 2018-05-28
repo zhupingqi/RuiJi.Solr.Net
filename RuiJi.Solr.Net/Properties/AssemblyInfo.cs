6b3ecf8dd05976a05d9dc307a2403f01f9fa833a
zhupingqi <416803633@qq.com>
2018-05-29 06:54:36 +0800

    assembly

--- RuiJi.Solr.Net/Properties/AssemblyInfo.cs
+++ RuiJi.Solr.Net/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Regards.Solr.Net")]
-[assembly: AssemblyDescription("Apache Solr .Net Client")]
+[assembly: AssemblyTitle("RuiJi.Solr.Net")]
+[assembly: AssemblyDescription("Apache solr client for .Net, The package contains commonly used solr requests and handler, and can be added on demand.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("zhupingqi")]
-[assembly: AssemblyProduct("Regards.Solr.Net")]
+[assembly: AssemblyProduct("RuiJi.Solr.Net")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
